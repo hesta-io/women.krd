4c562da8816c06b8b17c4108409982259632cbd1
Aram Rafeq <aramrafeq2@gmail.com>
2023-01-25 22:37:16 +0300

    initial changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 //  less setup https://medium.com/@Ibrabdh/next-js-and-antd-setup-with-less-support-29933468f93d
-const withLess = require('next-with-less');
 
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: false,
-  lessLoaderOptions: {},
 };
 
-module.exports = withLess(nextConfig);
+module.exports = nextConfig;
